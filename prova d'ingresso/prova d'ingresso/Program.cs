6fe527674d49f0d68ab82b10704c095dd7e62620
diegoalbertin <72445590+diegoalbertin@users.noreply.github.com>
2022-09-24 15:49:40 +0200

    coding completo

--- prova d'ingresso/prova d'ingresso/Program.cs
+++ prova d'ingresso/prova d'ingresso/Program.cs
@@ -10,26 +10,26 @@ namespace prova_d_ingresso
     {
         static void Main(string[] args)
         {
-            bool controllo;
-            double consumoCorrente, consumoGas, SMCTotali, KWhTotali;
+            bool controllo;                                             //dichiarazione delle variabili che verranno utilizzate per ii vari controlli
+            double consumoCorrente, consumoGas, SMCTotali, KWhTotali;   //e per gli inserimenti dei valori dei consumi
             string tmp;
             int sceltaMetodo;
 
-            do
+            do                                                                          //ciclo do while per l'inserimento del consumo di gas
             {
                 Console.WriteLine("inserisci i consumi di gas espressi in SMC\n");
                 tmp = Convert.ToString(Console.ReadLine());
                 controllo = double.TryParse(tmp, out consumoGas);
             } while (controllo == false || consumoGas < 0);
 
-            do
+            do                                                                         //ciclo do while per l'inserimento del consumo di corrente
             {
                 Console.WriteLine("inserisci i consumi di corrente espressi in KWh\n");
                 tmp = Convert.ToString(Console.ReadLine());
                 controllo = double.TryParse(tmp, out consumoCorrente);
             } while (controllo == false || consumoCorrente < 0);
 
-            do//metodo di riscaldamento
+            do                                                                         //ciclo do while per l'inserimento del metodo di riscaldamento
             {
                 Console.WriteLine("\nscegli il metodo di riscaldamento :\n" +
                               "Premi 1 -> la caldaia tradizionale\n" +
@@ -41,9 +41,9 @@ namespace prova_d_ingresso
                 controllo = int.TryParse(tmp, out sceltaMetodo);
             } while (controllo == false || sceltaMetodo > 5 || sceltaMetodo < 1);
 
-            SMCTotali = (consumoCorrente / 10.7) + consumoGas;
+            SMCTotali = (consumoCorrente / 10.7) + consumoGas;                      //calcolo smc e kwh totali
             KWhTotali = (consumoGas * 10.7) + consumoCorrente;
-            caldaiaCondensazione caldaiaCondensazione = new caldaiaCondensazione();
+            caldaiaCondensazione caldaiaCondensazione = new caldaiaCondensazione(); //dichiarazione delle varie classi e oggetti
             caldaiaTradizionale caldaiaTradizionale = new caldaiaTradizionale();
             PompaCaloreEconomica pompaCaloreEconomica = new PompaCaloreEconomica();
             PompaCaloreBuona pompaCaloreBuona = new PompaCaloreBuona();
@@ -67,11 +67,11 @@ namespace prova_d_ingresso
                     metodoRiscaldamento = stufaElettrica;
                     break;
             }
-            List<object> metodiRiscaldamento;
-            metodiRiscaldamento = new List<object>() { caldaiaTradizionale, caldaiaCondensazione, pompaCaloreBuona, pompaCaloreEconomica, stufaElettrica }; // Lista che contiene tutti i metodi di riscaldamento
+            //dichiarazione lista contenente i vari metodi di riscaldamento 
+            List<object> metodiRiscaldamento = new List<object>() { caldaiaTradizionale, caldaiaCondensazione, pompaCaloreBuona, pompaCaloreEconomica, stufaElettrica }; // Lista che contiene tutti i metodi di riscaldamento
             Bolletta bolletta;
             List<Bolletta> bolletteRiscaldamento = new List<Bolletta>();
-            foreach (Riscaldamento element in metodiRiscaldamento)
+            foreach (Riscaldamento element in metodiRiscaldamento)      //vengono create le bollette per ogni metodo di riscaldamento
             {
                 if (element.GetMateriaPrima() == "Corrente")
                 {
@@ -90,19 +90,19 @@ namespace prova_d_ingresso
                 bolletta.SetMetodoRiscaldamento(element);
                 bolletteRiscaldamento.Add(bolletta);
             }
-            foreach (Bolletta element in bolletteRiscaldamento)
+            foreach (Bolletta element in bolletteRiscaldamento)//vengono calcolate le bollette
             {
                 element.CalcolaBolletta();
             }
 
             bolletteRiscaldamento = bolletteRiscaldamento.OrderBy(b => b.GetTotale()).ToList(); // Viene ordinata la lista di bollette tramite il totale di ogni bolletta
 
-            foreach (Bolletta element in bolletteRiscaldamento)
+            foreach (Bolletta element in bolletteRiscaldamento)     //vengono elencati i vari metodi e le relative bollette
             {
                 Console.WriteLine($"{element.ToString()}");
             }
 
-            if (sceltaMetodo.ToString() == bolletteRiscaldamento[0].GetMetodoRiscaldamento().ToString())
+            if (sceltaMetodo.ToString() == bolletteRiscaldamento[0].GetMetodoRiscaldamento().ToString())            //suggerimento della scelta migliore all'utente
             {
                 Console.Write("\n\nIl metodo attualmente installato è il più conveniente\n");
             }
@@ -111,8 +111,6 @@ namespace prova_d_ingresso
                 Console.WriteLine($"\n\nL'offerta più conveniente è la seguente: {bolletteRiscaldamento[0].ToString()}");
             }
 
-            Console.ReadKey();
-            Console.WriteLine("{0}", metodoRiscaldamento.GetCostoInstallazione());
             Console.ReadKey();
         }
     }
